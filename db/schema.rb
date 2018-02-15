a2d59b9d89db150aaee764096aac7eaf23c06c34
Jon Norton <jmichaeln5@gmail.com>
2018-02-14 20:57:39 -0500

    Scaffolded Review and gave ability to comment, limited each Review to Product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180214224920) do
+ActiveRecord::Schema.define(version: 20180214234455) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,4 +23,13 @@ ActiveRecord::Schema.define(version: 20180214224920) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.text "comment"
+    t.bigint "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_reviews_on_product_id"
+  end
+
+  add_foreign_key "reviews", "products"
 end
